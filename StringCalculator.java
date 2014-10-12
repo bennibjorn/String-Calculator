beee385819d3cb317d61394525a004f3a6e289e4
Benjamin Hinriksson <bennibjorn@hotmail.com>
2014-10-12 16:34:19 +0000

    Wrote code for the tests, Add now handles an unspecified number of numbers.

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,17 +1,32 @@
 package stringcalc;
 
+import edu.princeton.cs.introcs.*;
+
 public class StringCalculator {
 
 	public static void main(String[] args) {
-		
-
+		String text = StdIn.readString();
+		StdOut.println(add(text));
 	}
 	public static int add(String numbers)
 	{
 		if (numbers.equals("")) {
 			return 0;
 		}
+		else if (numbers.contains(",")) {
+			String[] splitnumbers = split(numbers);
+			int sum = 0;
+			
+			for (int i = 0; i < splitnumbers.length; i++) {
+				sum += Integer.parseInt(splitnumbers[i]);
+			}
+			
+			return sum;
+		}
 		return 1;
+	}
+	private static String[] split(String numbers) {
+		return numbers.split(",");
 	}
 
 }
