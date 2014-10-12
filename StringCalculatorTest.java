b6b18edbf0e19311b6e198755a0127e6015024e2
Benjamin Hinriksson <bennibjorn@hotmail.com>
2014-10-12 16:39:24 +0000

    Wrote 2 more tests - 1st kata step solved

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -15,7 +15,15 @@ public class StringCalculatorTest {
 		assertEquals(1, StringCalculator.add("1"));
 	}
 	@Test
+	public void testSingleNumber2() {
+		assertEquals(50, StringCalculator.add("50"));
+	}
+	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, StringCalculator.add("1,2"));
+	}
+	@Test
+	public void testTwoNumbers2() {
+		assertEquals(12, StringCalculator.add("6,6"));
 	}
 }
