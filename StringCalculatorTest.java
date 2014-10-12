ea1155fd0def6915dc14543ace967d4caa39fc21
Benjamin Hinriksson <bennibjorn@hotmail.com>
2014-10-12 16:17:52 +0000

    Wrote two test cases for single and two numbers

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void testAdd() {
+	public void testEmptyString() {
 		assertEquals(0, StringCalculator.add(""));
 	}
-
+	public void testSingleNumber() {
+		assertEquals(1, StringCalculator.add("1"));
+	}
+	public void testTwoNumbers() {
+		assertEquals(3, StringCalculator.add("1,2"));
+	}
 }
