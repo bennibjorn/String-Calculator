611650f6ed470fa7df391d12f8349a217db84252
Benjamin Hinriksson <bennibjorn@hotmail.com>
2014-10-13 17:25:36 +0000

    Got a bit deep into regex to solve 4th kata step, Added a sum method to simplify the code

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,5 +1,8 @@
 package stringcalc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import edu.princeton.cs.introcs.*;
 
 public class StringCalculator {
@@ -8,25 +11,39 @@ public class StringCalculator {
 		String text = StdIn.readString();
 		StdOut.println(add(text));
 	}
-	public static int add(String numbers)
-	{
+	
+	public static int add(String numbers) {
 		if (numbers.equals("")) {
 			return 0;
 		}
+		else if (numbers.startsWith("//")) {
+			return sum(specificDelimiter(numbers));
+		}
 		else if (numbers.contains(",") || numbers.contains("/n")) {
-			String[] splitnumbers = split(numbers);
-			int sum = 0;
-			
-			for (int i = 0; i < splitnumbers.length; i++) {
-				sum += Integer.parseInt(splitnumbers[i]);
-			}
-			
-			return sum;
+			return sum(split(numbers));
 		}
 		else {
 			return Integer.parseInt(numbers);
 		}
 	}
+	// Calculating the sum ---------------------------
+	private static int sum(String[] stringnumbers) {
+		int sum = 0;
+		for (int i = 0; i < stringnumbers.length; i++) {
+			sum += Integer.parseInt(stringnumbers[i]);
+		}
+		return sum;
+	}
+	// If there is a specific delimiter, determine the delimiter using regex
+	// and split using that specific delimiter
+	private static String[] specificDelimiter(String specific) {		
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(specific);
+		m.matches();
+		String delimiter = m.group(1);
+		String numbers = m.group(2);
+		return numbers.split(delimiter);
+	}
+	// Standard split with comma or newline (or both)
 	private static String[] split(String numbers) {
 		String[] split = null;
 		
